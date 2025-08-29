e1b1a4fcda66cd86bac038fab6aa93fe2d57bb7e
Alok Rai <krai.alok@gmail.com>
2025-08-29 22:56:00 +0530

    first app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          <code>Welcome to Home Interior World</code>
+          <p> Build your Home.</p> 
+        </p>
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Requirement
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
